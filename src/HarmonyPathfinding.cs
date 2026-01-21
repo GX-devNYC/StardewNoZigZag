c34fcd3609c5f25c7789261836a41ff59ce2ceb9
GX-devNYC <gavinxie@buffalo.edu>
2026-01-20 23:07:54 -0500

    Add project files.

---
+++ src/HarmonyPathfinding.cs
@@ -0,0 +1,113 @@
+﻿using HarmonyLib;
+using NoZigZag.API;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Pathfinding;
+using StardewValley.TerrainFeatures;
+using System.Diagnostics.Metrics;
+using static StardewValley.Pathfinding.PathFindController;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.Xna.Framework;
+using StardewValley.Audio;
+using StardewValley.Extensions;
+using StardewValley.Locations;
+using xTile.Tiles;
+
+namespace NoZigZag.src
+{
+	public class HarmonyPathfinding
+	{
+		public static IMonitor Monitor;
+
+		// call this method from Entry class
+		internal static void Initialize(IMonitor monitor)
+		{
+			Monitor = monitor;
+		}
+
+		protected static int _counterH = 0;
+		protected static PriorityQueue _openListH = new PriorityQueue();
+		protected static HashSet<int> _closedListH = new HashSet<int>();
+		protected static readonly sbyte[,] DirectionsH = new sbyte[4, 2]
+			{
+					{ -1, 0 },
+					{ 1, 0 },
+					{ 0, 1 },
+					{ 0, -1 }
+			};
+
+		public static Stack<Point> findPath_prefix(Point startPoint, Point endPoint, isAtEnd endPointFunction, GameLocation location, Character character, int limit)
+		{
+			// ????
+			if (Interlocked.Increment(ref _counterH) != 1)
+			{
+				throw new Exception();
+			}
+			try
+			{
+				bool ignore_obstructions = character is FarmAnimal animal && animal.CanSwim() && animal.isSwimming.Value;
+				_openListH.Clear();
+				_closedListH.Clear();
+				PriorityQueue openList = _openListH;
+				HashSet<int> closedList = _closedListH;
+				int iterations = 0;
+				openList.Enqueue(new PathNode(startPoint.X, startPoint.Y, 0, null), Math.Abs(endPoint.X - startPoint.X) + Math.Abs(endPoint.Y - startPoint.Y));
+				int layerWidth = location.map.Layers[0].LayerWidth;
+				int layerHeight = location.map.Layers[0].LayerHeight;
+				int facing_direction = character.FacingDirection;
+				while (!openList.IsEmpty())
+				{
+					PathNode currentNode = openList.Dequeue();
+					if (endPointFunction(currentNode, endPoint, location, character))
+					{
+						return reconstructPath(currentNode);
+					}
+					closedList.Add(currentNode.id);
+					int ng = (byte)(currentNode.g + 1);
+					for (int i = 0; i < 4; i++)
+					{
+						int nx = currentNode.x + DirectionsH[i, 0];
+						int ny = currentNode.y + DirectionsH[i, 1];
+						int nid = PathNode.ComputeHash(nx, ny);
+						if (closedList.Contains(nid))
+						{
+							continue;
+						}
+						if ((nx != endPoint.X || ny != endPoint.Y) && (nx < 0 || ny < 0 || nx >= layerWidth || ny >= layerHeight))
+						{
+							closedList.Add(nid);
+							continue;
+						}
+						PathNode neighbor = new PathNode(nx, ny, currentNode);
+						neighbor.g = (byte)(currentNode.g + 1);
+						if (!ignore_obstructions && location.isCollidingPosition(new Rectangle(neighbor.x * 64 + 1, neighbor.y * 64 + 1, 62, 62), Game1.viewport, character is Farmer, 0, glider: false, character, pathfinding: true))
+						{
+							closedList.Add(nid);
+							continue;
+						}
+						int f = ng + (Math.Abs(endPoint.X - nx) + Math.Abs(endPoint.Y - ny));
+						closedList.Add(nid);
+						openList.Enqueue(neighbor, f);
+					}
+					iterations++;
+					if (iterations >= limit)
+					{
+						return null;
+					}
+				}
+				return null;
+			}
+			finally
+			{
+				if (Interlocked.Decrement(ref _counterH) != 0)
+				{
+					throw new Exception();
+				}
+			}
+		}
+
+	}
+}
---
+++ src/ModEntry.cs
@@ -0,0 +1,61 @@
+﻿using HarmonyLib;
+using NoZigZag.API;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Pathfinding;
+
+namespace NoZigZag.src
+{
+	internal sealed class ModEntry : Mod
+{
+		public ModConfig Config;
+
+		public override void Entry(IModHelper helper)
+		{
+			this.Config = this.Helper.ReadConfig<ModConfig>();
+			helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+		}
+
+		// when game is launched: setup the config menu
+		public void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+		{
+			SetConfigMenu(sender, e);
+		}
+
+		public void HarmonyPatching()
+		{
+			Harmony harmony = new(ModManifest.UniqueID);
+
+			harmony.Patch(
+				original: AccessTools.Method(typeof(PathFindController), nameof(PathFindController.findPath)),
+				prefix: new HarmonyMethod(typeof(HarmonyPathfinding), nameof(HarmonyPathfinding.findPath_prefix))
+			);
+		}
+
+		// Generic Config Menu setup
+		public void SetConfigMenu(object? sender, GameLaunchedEventArgs e)
+		{
+			// get Generic Mod Config Menu's API (if it's installed)
+			var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+			if (configMenu is null)
+				return;
+
+			// register mod
+			configMenu.Register(
+					mod: this.ModManifest,
+					reset: () => this.Config = new ModConfig(),
+					save: () => this.Helper.WriteConfig(this.Config)
+			);
+
+			// add some config options
+			configMenu.AddBoolOption(
+					mod: this.ModManifest,
+					getValue: () => this.Config.ModEnabled,
+					setValue: value => this.Config.ModEnabled = value,
+					name: () => "Allow mod to disable NPC zigzagging",
+					tooltip: () => "This is Stardew NoZigZag's entire function."
+			);
+		}
+	}
+}
